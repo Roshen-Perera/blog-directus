20ab6086327be424702525fe1c075aaff9ddabe6
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-04-03 16:04:29 +0530

    refactor: remove type annotation for post in posts mapping

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Page() {
       <h1 className="text-4xl font-bold mb-8">Directus Blog</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {posts.map((post: any) => (
+        {posts.map((post) => (
           <PostCard key={post.id} post={post} />
         ))}
       </div>
